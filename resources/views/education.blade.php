e8473ce574fae09ff6da2b0a836ae07a7cf2bd89
tanizinnat <tani15-5315@diu.edu.bd>
2025-06-26 16:09:16 +0600

    protfolio 2

--- resources/views/education.blade.php
+++ resources/views/education.blade.php
@@ -1,10 +1,12 @@
 @extends('layout')
 @section('title', 'Education')
 @section('content')
-<h1>Education</h1>
-<ul>
-  <li>BSc in Computer Science and Engineering - Daffodil International University (2021 - Present)</li>
-  <li>HSC - Science - XYZ College</li>
-  <li>SSC - Science - ABC School</li>
-</ul>
+<div class="education-container">
+  <h1>Education</h1>
+  <ul class="education-list">
+    <li><strong>BSc in CSE</strong> - Daffodil International University (2021 - Present)</li>
+    <li><strong>HSC</strong> - Milestone College, Uttara</li>
+    <li><strong>SSC</strong> - Sristy Academic School</li>
+  </ul>
+</div>
 @endsection
